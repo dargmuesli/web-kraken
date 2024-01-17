897799b6ad72b931a996ae4ada640f104eb0881b
JulianOpper <opper.julian@gmx.de>
2024-01-17 20:29:23 +0100

    feat: add imported globals typings

--- apps/analyzer/src/entity/global.ts
+++ apps/analyzer/src/entity/global.ts
@@ -1,6 +1,7 @@
 export class Global {
     constructor(
         private readonly name: string,
+        private readonly type?: string,
         private readonly source?: string,
         private readonly mutable?: boolean
     ) {}
@@ -15,5 +16,9 @@ export class Global {
 
     public getMutable(): boolean | undefined {
         return this.mutable;
+    }
+
+    public getType(): string | undefined {
+        return this.type;
     }
 }
