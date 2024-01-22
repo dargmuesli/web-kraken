1869c2eb97537a2360fbbf52b2c65a6961bbc276
JulianOpper <opper.julian@gmx.de>
2024-01-22 16:57:57 +0100

    fix: ignore files without a data segment and check for passive data segments

--- apps/analyzer/src/commands/datadump.ts
+++ apps/analyzer/src/commands/datadump.ts
@@ -7,11 +7,12 @@ import { Data } from '../entity/data';
 export async function datadump(file: string, options: OptionValues) {
     const result = await getCommandResult('wasm-objdump', ['./' + file, '-x', '-j', 'Data']);
     const dataString = result.split(/Data\[[0-9]+]:/g)[1];
+    if (!dataString) return;
     const lines = dataString.split(/\n/);
 
     let currentSize = -1;
     let currentData = '';
-    let currentMemory = -1;
+    let currentMemory: number | undefined = -1;
     const segments: Data[] = [];
 
     lines.forEach((line) => {
@@ -24,7 +25,8 @@ export async function datadump(file: string, options: OptionValues) {
             }
             // - segment[0] memory=0 size=13972 - init i32=1024
             currentSize = parseInt(line.split('size=')[1].split(' ')[0]);
-            currentMemory = parseInt(line.split('memory=')[1].split(' ')[0]);
+            const part = line.split('memory=')[1];
+            currentMemory = part ? parseInt(part.split(' ')[0]) : undefined;
             return;
         }
         const diff = Math.min(currentSize, 16);
